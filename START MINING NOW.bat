e0b1e783ea8b770717632af851018fe78815d137
John Hudson <31929115+SOFTSYNX@users.noreply.github.com>
2019-09-13 11:23:22 -0700

    start mining now
    bat.file

---
+++ START MINING NOW.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+X64_70.exe -zpool ssl://zec-eu1.nanopool.org:6633 -zwal t1dtY9Fdq5GYT7orvEvZka21SQkwG3V6dkR.FAST_BTC_MINER -zpsw z -ftime 1
+pause
